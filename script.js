23cda032e479bbdd2f59f1d9998584f625e27bfd
nicksandoval <sand8val@gmail.com>
2024-05-12 20:25:03 -0500

    more styling

--- script.js
+++ script.js
@@ -12,9 +12,13 @@ function generateBoard() {
         row.className = "row";
         for (let j = 0; j < board[i].length; j++) {
             let square = document.createElement("div");
+            let coordinates = document.createElement("span");
+            coordinates.style.position = "absolute";
+            coordinates.innerText = board[i][j];
             square.className = "square";
             square.id = board[i][j];
-            square.innerText = board[i][j];
+            square.append(coordinates);
+            
 
             if ((j + i) % 2 == 0) {
                 square.classList.add("black"); 
@@ -25,9 +29,24 @@ function generateBoard() {
     }
 }
 
+window.addEventListener("click", function(e) {
+    const target = e.target;
+    if (target.classList.contains("black")) {
+        const knightElement = document.createElement("img");
+        knightElement.src = "./knight.png";
+        target.append(knightElement);
+
+        generateKnightsTour(target.id);
+    }
+})
+
+function generateKnightsTour(coordinates) {
+    // const generate = new GenerateTour(5, coordinates);
+    // const tour = generate.knightsTour
+    
+}
 
 
 
 generateBoard();
-// console.log(board.board);
 
