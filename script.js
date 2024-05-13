e51721a9652f57a70a6a80bbe60ab441db3d70c4
nicksandoval <sand8val@gmail.com>
2024-05-12 20:56:38 -0500

    fixing styles

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 
 
 
-
+let gameOn = false;
 
 function generateBoard() {
     const boardElement = document.getElementById("board");
+    boardElement.innerHTML = "";
     const board = new Board(5).board;
-    console.log(board);
+    const instructions = document.getElementById("instructions");
+        instructions.style.display = "block";
+    
     for (let i = 0; i < board.length; i++) {
         let row = document.createElement("div");
         row.className = "row";
@@ -31,22 +34,49 @@ function generateBoard() {
 
 window.addEventListener("click", function(e) {
     const target = e.target;
-    if (target.classList.contains("black")) {
-        const knightElement = document.createElement("img");
-        knightElement.src = "./knight.png";
-        target.append(knightElement);
-
+    if (target.classList.contains("black") && !gameOn) {
+        gameOn = true;
+        const instructions = document.getElementById("instructions");
+        instructions.style.display = "none";
         generateKnightsTour(target.id);
     }
 })
 
-function generateKnightsTour(coordinates) {
-    // const generate = new GenerateTour(5, coordinates);
-    // const tour = generate.knightsTour
-    
+function plotKnightElement(targetElement) {
+    const knightElement = document.createElement("img");
+    knightElement.src = "./knight.png";
+    targetElement.append(knightElement);
 }
 
+function generateKnightsTour(coordinates) {
+    const generate = new GenerateTour(5, coordinates);
+    const tour = generate.knightsTour;
+    animateTour(tour);
+}
 
+function animateTour(tourPlots) {
+    let counter = 0;
+    let interval = setInterval(() => {
+        if (counter > tourPlots.length - 1) {
+            clearInterval(interval);
+            populateResetButton();
+            return;
+        }
+        const sqaure = document.getElementById(tourPlots[counter]);
+        plotKnightElement(sqaure);
+        counter++;
+    }, 250);
+}
+
+function populateResetButton() {
+    const resetBtn = document.getElementById("resetBtn");
+    resetBtn.style.display = "block";
+    resetBtn.addEventListener("click", function(e) {
+        gameOn = true;
+        resetBtn.style.display = "none";
+        generateBoard();
+    });
+}
 
 generateBoard();
 
